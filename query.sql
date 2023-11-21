760a4c643d94d23431f48153d646b0c9ac0c252a
jeremy-mazzocco <jeremyozz@yahoo.it>
2023-11-21 14:51:00 +0100

    first five

---
+++ query.sql
@@ -0,0 +1,11 @@
+1. SELECT * FROM `software_houses`;
+
+2. SELECT * FROM `players` WHERE `city` = 'Rogahnland';
+
+3. SELECT * FROM `players` WHERE `name` LIKE '%a';
+
+4. SELECT * FROM `reviews` WHERE `player_id` = 800;
+
+5. SELECT COUNT(*) FROM `tournaments` WHERE `year` =  2015;
+
+6. 
