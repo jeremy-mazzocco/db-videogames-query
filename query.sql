2472086cda2e1f88001ebe356e4e979e3e7f29f5
jeremy-mazzocco <jeremyozz@yahoo.it>
2023-11-21 15:31:45 +0100

    more 10

--- query.sql
+++ query.sql
@@ -8,4 +8,12 @@
 
 5. SELECT COUNT(*) FROM `tournaments` WHERE `year` =  2015;
 
-6. 
+6. SELECT * FROM `awards` WHERE `description` LIKE '%facere%';
+
+7. SELECT * FROM `category_videogame` WHERE `category_id` = 2 OR `category_id` = 6;
+
+8. SELECT * FROM `reviews` WHERE `rating` BETWEEN 2 AND 4;
+
+9. SELECT * FROM `videogames` WHERE YEAR(release_date) = 2020;
+
+10. SELECT DISTINCT `videogame_id` FROM `reviews` WHERE `rating` = 5 ORDER BY `reviews`.`videogame_id` ASC;
